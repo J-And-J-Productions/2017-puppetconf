077b979482c9e180fd1dabcfb17e893c23398a1e
Jessie Puls <jessie.puls@dhigroupinc.com>
2017-10-09 18:05:08 -0700

    Adding net/http require

--- environment/test/serverspec/web_server_spec.rb
+++ environment/test/serverspec/web_server_spec.rb
@@ -1,27 +1,28 @@
 require_relative 'serverspec_helper'
 require 'aws_serverspec_helpers'
+require 'net/http'
 
 set :host, 'example1.pupperlabs.com'
 
 describe 'web server' do
   describe package('puppet-agent') do
-    it { should be_installed }
+    it {should be_installed}
   end
 
   describe service('apache2') do
-    it { should be_running }
-    it { should be_enabled }
+    it {should be_running}
+    it {should be_enabled}
   end
 
   describe port(80) do
-    it { should be_listening }
+    it {should be_listening}
   end
 
   it 'should successfully call the test endpoint' do
     uri = URI "http://example1.pupperlabs.com"
     response = Net::HTTP.get_response uri
     expect(response.code).to eql '200'
-	expect(response.body).to contain('A generic webpage')
+    expect(response.body).to contain('A generic webpage')
   end
 
 end
