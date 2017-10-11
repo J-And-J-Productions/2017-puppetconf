abf41dc0ac670408fa91ac9f538cd961d9ea864c
Jesus Alvarez <jesus.alvarez@dhigroupinc.com>
2017-10-11 12:41:02 -0700

    Remove unused port 81

--- environment/basic_setup.tf
+++ environment/basic_setup.tf
@@ -127,13 +127,6 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ingress {
-    # from_port   = 81
-    # to_port     = 81
-    # protocol    = "tcp"
-    # cidr_blocks = ["0.0.0.0/0"]
-  # }
-
   egress {
     from_port   = 0
     to_port     = 65535
