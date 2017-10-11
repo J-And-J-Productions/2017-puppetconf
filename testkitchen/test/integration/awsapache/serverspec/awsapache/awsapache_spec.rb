fed243c13feccdb782edbdde4c00078b90375dfe
Jesus Alvarez <jesus.alvarez@dhigroupinc.com>
2017-10-11 12:29:40 -0700

    Fix test kitchen test

--- testkitchen/test/integration/awsapache/serverspec/awsapache/awsapache_spec.rb
+++ testkitchen/test/integration/awsapache/serverspec/awsapache/awsapache_spec.rb
@@ -5,9 +5,9 @@ describe service('apache2') do
   it { should be_enabled }
 end
 
-describe file('/etc/apache2/sites-enabled/25-www.example-1.com.conf') do
+describe file('/etc/apache2/sites-enabled/25-example1.conf') do
   it { should exist }
-  it { should contain 'ServerName www.example-1.com' }
+  it { should contain 'ServerName example1'}
 end
 
 describe file('/etc/apache2/ports.conf') do
